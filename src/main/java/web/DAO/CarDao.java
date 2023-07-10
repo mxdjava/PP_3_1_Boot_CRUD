40d8e510201e6b1e5fc3dbed1331eba5974107b1
mxdjava <charismaticdrugger@gmail.com>
2023-07-10 19:30:27 +0600

    Поменял CarService на CarDao

--- src/main/java/web/DAO/CarDao.java
+++ src/main/java/web/DAO/CarDao.java
@@ -1,19 +1,30 @@
-package web.Service;
+package web.DAO;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CarService {
+@Component
+public class CarDao {
+    private List<Car> cars;
 
-    private List<Car> cars = new ArrayList<>();
-    public List<Car> getCars() {
+    {
+        cars = new ArrayList<>();
         cars.add(new Car("e200", "Mercedes"));
         cars.add(new Car("GLK", "Mercedes"));
         cars.add(new Car("L200", "Mitsubishi"));
         cars.add(new Car("Optima", "KIA"));
         cars.add(new Car("Patriot", "UAZ"));
+    }
+
+    public List<Car> getCars() {
         return cars;
+    }
+
+    public List<Car> numCars(int num) {
+        List<Car> subCars = cars.subList(0, num);
+        return subCars;
     }
 }
--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,14 +1,30 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import web.DAO.CarDao;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-    @GetMapping
-    public String getCars() {
+    private final CarDao carDao;
+
+    public CarController(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+    @GetMapping()
+    public String AllCars(Model model) {
+        model.addAttribute("cars", carDao.getCars());
         return "cars";
+    }
+    @GetMapping("?count={num}")
+    public String numOut (@PathVariable("num") int num, Model model) {
+        model.addAttribute("cars", carDao.numCars(num));
+        return "cars?count={num}";
     }
 }
